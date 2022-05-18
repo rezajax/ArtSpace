000c3ea455ea4d41255130d52d8a813a1b6178a3
Reza Jax <rezajaxg@gmail.com>
2022-05-18 15:43:57 +0430

    add BoxWithConstraints and if (maxWidth < 600.dp) | if (maxWidth >= 600.dp)

--- app/src/main/java/app/bidar/artspace/MainActivity.kt
+++ app/src/main/java/app/bidar/artspace/MainActivity.kt
@@ -24,6 +24,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+
+
             ArtSpaceTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
@@ -42,94 +44,194 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ArtSpaceApp() {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Top,
-        horizontalAlignment = Alignment.CenterHorizontally
 
-    ) {
-        Spacer(modifier = Modifier.weight(1f))
-
-        Card(
-            modifier = Modifier//.fillMaxWidth(.5f).fillMaxHeight(.5f)
-                .padding(16.dp)
-                .shadow(1.dp)
-                .border(BorderStroke(3.dp, Color(R.color.border)))
-            //.height(600.dp)
-        ) {
-            Image(
-
-                painter = painterResource(id = R.drawable.takhte_jamshid),
-                contentDescription = null,
-                modifier = Modifier
-                    .padding(16.dp)
-
-            )
-        }
-
-        Spacer(modifier = Modifier.weight(1f))
-
-        Card(modifier = Modifier
-            .padding(16.dp)
-            .shadow(clip = true, elevation = 2.dp)
-        ) {
+    BoxWithConstraints {
+        if (maxWidth < 600.dp) {
             Column(
-                modifier = Modifier
-                    .padding(16.dp)
-                   // .fillMaxWidth(.5f).fillMaxHeight(.5f)
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+
             ) {
-                Text(
-                    modifier = Modifier
-                        .padding(0.dp),
-                    text = "Tagh Bostan",
-                    fontSize = 25.sp,
+                Spacer(modifier = Modifier.weight(1f))
+
+                Card(
+                    modifier = Modifier//.fillMaxWidth(.5f).fillMaxHeight(.5f)
+                        .padding(16.dp)
+                        .shadow(1.dp)
+                        .border(BorderStroke(3.dp, Color(R.color.border)))
+                    //.height(600.dp)
+                ) {
+                    Image(
+
+                        painter = painterResource(id = R.drawable.takhte_jamshid),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .padding(16.dp)
 
                     )
-                Row() {
-                    Text(
-                        modifier = Modifier
-                            .padding(0.dp),
-                        text = "Kermanshah ",
-                        fontWeight = FontWeight.Bold
-                    )
-                    Text(
-                        modifier = Modifier
-                            .padding(0.dp),
-
-                        text = "(1991)",
-
-                        )
                 }
 
-            }
+                Spacer(modifier = Modifier.weight(1f))
 
+                Card(
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .shadow(clip = true, elevation = 2.dp)
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .padding(16.dp)
+                        // .fillMaxWidth(.5f).fillMaxHeight(.5f)
+                    ) {
+                        Text(
+                            modifier = Modifier
+                                .padding(0.dp),
+                            text = "Tagh Bostan",
+                            fontSize = 25.sp,
+
+                            )
+                        Row() {
+                            Text(
+                                modifier = Modifier
+                                    .padding(0.dp),
+                                text = "Kermanshah ",
+                                fontWeight = FontWeight.Bold
+                            )
+                            Text(
+                                modifier = Modifier
+                                    .padding(0.dp),
+
+                                text = "(1991)",
+
+                                )
+                        }
+
+                    }
+
+                }
+
+
+                Row(
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .fillMaxWidth()
+                        .align(alignment = Alignment.End)
+                        .align(Alignment.CenterHorizontally)
+                ) {
+
+                    Button(onClick = { /*TODO*/ }, modifier = Modifier.padding(16.dp)) {
+                        Text(text = "Previous")
+                    }
+
+                    Spacer(modifier = Modifier.weight(1f))
+
+                    Button(onClick = { /*TODO*/ }, modifier = Modifier.padding(16.dp)) {
+                        Text(text = "Next")
+                    }
+
+
+                }
+
+
+            }
         }
 
+        if (maxWidth >= 600.dp) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
 
-        Row(
-            modifier = Modifier
-                .padding(16.dp)
-                .fillMaxWidth()
-                .align(alignment = Alignment.End)
-                .align(Alignment.CenterHorizontally)
-        ) {
+            ) {
+                //Spacer(modifier = Modifier.weight(1f))
 
-            Button(onClick = { /*TODO*/ }, modifier = Modifier.padding(16.dp)) {
-                Text(text = "Previous")
+                Card(
+                    modifier = Modifier//.fillMaxWidth(.5f).fillMaxHeight(.5f)
+                        .padding(16.dp)
+                        .shadow(1.dp)
+                        .border(BorderStroke(3.dp, Color(R.color.border)))
+                        .weight(1f)
+                ) {
+                    Image(
+
+                        painter = painterResource(id = R.drawable.takhte_jamshid),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .padding(16.dp)
+                            .wrapContentWidth()
+                            .width(700.dp)
+
+                    )
+                }
+
+                //Spacer(modifier = Modifier.weight(1f))
+
+                Card(
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .shadow(clip = true, elevation = 2.dp)
+                        .weight(.3f)
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .padding(16.dp)
+                         //.fillMaxWidth(.5f).fillMaxHeight(.5f)
+                    ) {
+                        Text(
+                            modifier = Modifier
+                                .padding(0.dp),
+                            text = "Tagh Bostan",
+                            fontSize = 25.sp,
+
+                            )
+                        Row() {
+                            Text(
+                                modifier = Modifier
+                                    .padding(0.dp),
+                                text = "Kermanshah ",
+                                fontWeight = FontWeight.Bold
+                            )
+                            Text(
+                                modifier = Modifier
+                                    .padding(0.dp),
+
+                                text = "(1991)",
+
+                                )
+                        }
+
+                    }
+
+                }
+
+
+                Row(
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .fillMaxWidth()
+                        .align(alignment = Alignment.End)
+                        .align(Alignment.CenterHorizontally)
+                ) {
+
+                    Button(onClick = { /*TODO*/ }, modifier = Modifier.padding(16.dp)) {
+                        Text(text = "Previous")
+                    }
+
+                    Spacer(modifier = Modifier.weight(1f))
+
+                    Button(onClick = { /*TODO*/ }, modifier = Modifier.padding(16.dp)) {
+                        Text(text = "Next")
+                    }
+
+
+                }
+
+
             }
-
-            Spacer(modifier = Modifier.weight(1f))
-
-            Button(onClick = { /*TODO*/ }, modifier = Modifier.padding(16.dp)) {
-                Text(text = "Next")
-            }
-
-
         }
-
-
-
     }
+
 }
 
 
@@ -137,10 +239,15 @@ fun ArtSpaceApp() {
 @Preview(name= "dark")
 
 @Preview(showSystemUi = true)
-@Preview(showBackground = true, name= "tablet 10", heightDp = 1600, widthDp = 2560)
+
+@Preview(showBackground = true, name= "tablet 3", heightDp = 600, widthDp = 900)
+@Preview(showBackground = true, name= "tablet 3", heightDp = 900, widthDp = 600)
 
 @Preview(showBackground = true, name= "tablet 7", heightDp = 1200, widthDp = 1920)
-@Preview(showBackground = true, name= "tablet 3", heightDp = 600, widthDp = 900)
+@Preview(showBackground = true, name= "tablet 10", heightDp = 1600, widthDp = 2560)
+
+
+
 @Composable
 fun ArtSpacePreview() {
     ArtSpaceTheme {
